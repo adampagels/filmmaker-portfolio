1d5169e64dcf4e8a831cb98cd6c8af6afeaa768c
Adam Pagels <pagels@hotmail.ca>
2022-04-14 15:23:04 +0100

    Display thumbnails of all movies

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,47 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+// import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
+const Index = ({ data }) => {
+  const films = data.allContentfulVideoDetails.edges
+  console.log(data)
+  return (
+    <Layout>
+      <Seo title="Films" />
+      <h1>hey</h1>
+      {films &&
+        films.map(({ node: film }) => {
+          return (
+            <div>
+              <img
+                alt={film.filmTitle}
+                src={`${film.filmThumbnail.file.url}`}
+              />
+            </div>
+          )
+        })}
+    </Layout>
+  )
+}
+export default Index
+export const query = graphql`
+  query IndexPageQuery {
+    allContentfulVideoDetails {
+      edges {
+        node {
+          id
+          slug
+          filmTitle
+          filmThumbnail {
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
