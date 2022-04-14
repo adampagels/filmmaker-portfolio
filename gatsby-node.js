422d40e83156538807188a4df936c4facb7842e0
Adam Pagels <pagels@hotmail.ca>
2022-04-14 11:15:35 +0100

    Create pages based on Contentful content

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,41 @@
-exports.createPages = async ({ actions }) => {
+const path = require(`path`)
+const slash = require(`slash`)
+exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
+
+  return graphql(
+    `
+      {
+        allContentfulVideoDetails {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    `
+  )
+    .then(result => {
+      if (result.errors) {
+        console.log("Error retrieving contentful data", result.errors)
+      }
+
+      const filmTemplate = path.resolve("./src/templates/film.js")
+
+      result.data.allContentfulVideoDetails.edges.forEach(edge => {
+        createPage({
+          path: `/films/${edge.node.slug}/`,
+          component: slash(filmTemplate),
+          context: {
+            slug: edge.node.slug,
+            id: edge.node.id,
+          },
+        })
+      })
+    })
+    .catch(error => {
+      console.log("Error retrieving contentful data", error)
+    })
 }
