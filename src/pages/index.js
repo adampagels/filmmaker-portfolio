080d4a728b3eab6275367a15075e45665bfe76d0
Adam Pagels <pagels@hotmail.ca>
2022-04-15 12:24:06 +0100

    Update styling of thumbnails and container

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,26 @@ import * as React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
+import styled from "styled-components"
 import Seo from "../components/seo"
+
+const ImageWrapper = styled.div`
+  align-items: center;
+  background-color: black;
+  display: flex;
+  min-width: 100px;
+  max-width: 370px;
+  margin: 10px;
+  width: 80vw;
+`
+
+const FilmsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+const Image = styled.img``
 
 const Index = ({ data }) => {
   const films = data.allContentfulVideoDetails.edges
@@ -12,17 +31,24 @@ const Index = ({ data }) => {
     <Layout>
       <Seo title="Films" />
       <h1>hey</h1>
-      {films &&
-        films.map(({ node: film }) => {
-          return (
-            <div>
-              <img
-                alt={film.filmTitle}
-                src={`${film.filmThumbnail.file.url}`}
-              />
-            </div>
-          )
-        })}
+      <FilmsContainer>
+        {films &&
+          films.map(({ node: film }) => {
+            return (
+              <ImageWrapper>
+                <Image
+                  alt={film.filmTitle}
+                  src={`${film.filmThumbnail.file.url}`}
+                />
+              </ImageWrapper>
+            )
+          })}
+        <>
+          <ImageWrapper>
+            <img src={films[0].node.filmThumbnail.file.url} />
+          </ImageWrapper>
+        </>
+      </FilmsContainer>
     </Layout>
   )
 }
